10fa766db91af90b4bc6eba41f285a67562a3efa
Jorge Cuases <zappy2310@gmail.com>
2019-01-23 10:28:54 -0500

    Añadiendo joins

--- application/models/Cliente_model.php
+++ application/models/Cliente_model.php
@@ -6,7 +6,10 @@ class Cliente_model extends CI_Model{
 
 
   public function get(){
-    $query = $this->db->query("select*from tbl_cliente");
+    $query = $this->db->query("select c.id_cl,c.cedula_cl,c.nombres_cl,c.apellidos_cl,c.fecha_nacimiento_cl,c.telefono_cl,c.correo_cl,
+	 u.codigo_postal_ub, u.pais_ub text,u.provincia_ub,u.ciudad_ub,u.calle_uno_ub,u.calle_dos_ub,u.referencia_ub,u.numero_casa_ub
+	 from tbl_cliente c
+	 inner join tbl_ubicacion u on c.id_cl=u.id_cl");
 		if($query->num_rows() > 0){
 			return $query->result_array();
 		}
