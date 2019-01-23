3febd7c19d7e6b12b065ae47cda5e2899407c619
Jorge Cuases <zappy2310@gmail.com>
2019-01-23 11:32:26 -0500

    Error

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'cliente';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-
-$route['Cliente']['get'] = 'cliente/get';
