3b8129bc08b3f0f82265eb3a76b88812949fefc0
Jorge Cuases <zappy2310@gmail.com>
2019-01-23 10:43:05 -0500

    Error

--- application/models/Cliente_model.php
+++ application/models/Cliente_model.php
@@ -6,8 +6,7 @@ class Cliente_model extends CI_Model{
 
 
   public function get(){
-    $query = $this->db->query("select c.id_cl,c.cedula_cl,c.nombres_cl,c.apellidos_cl,c.fecha_nacimiento_cl,c.telefono_cl,c.correo_cl,
-	 u.codigo_postal_ub, u.pais_ub text,u.provincia_ub,u.ciudad_ub,u.calle_uno_ub,u.calle_dos_ub,u.referencia_ub,u.numero_casa_ub
+    $query = $this->db->query("select c.id_cl,c.cedula_cl,c.nombres_cl,c.apellidos_cl,c.fecha_nacimiento_cl,c.telefono_cl,c.correo_cl, u.codigo_postal_ub, u.pais_ub, u.provincia_ub,u.ciudad_ub,u.calle_uno_ub,u.calle_dos_ub,u.referencia_ub,u.numero_casa_ub
 	 from tbl_cliente as c
 	 join tbl_ubicacion  as u on c.id_cl=u.id_cl");
 		if($query->num_rows() > 0){
@@ -15,7 +14,6 @@ class Cliente_model extends CI_Model{
 		}
 		return null;
 	}
-
 
 
 
