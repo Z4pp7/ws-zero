19e6b218832201437427d0622c30edff3b1f2fb4
Jorge Cuases <zappy2310@gmail.com>
2019-01-23 15:45:24 -0500

    Cambio again

--- application/controllers/Cliente.php
+++ application/controllers/Cliente.php
@@ -2,14 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once APPPATH . 'libraries/REST_Controller.php';
 class Cliente extends REST_Controller {
+
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('cliente_model');
 	}
-	public function index_get(){
+
+
+	public function all_get(){
 		$clientes = $this->cliente_model->getClientes();
 		if (!is_null($clientes)) {
-			$this->response(array('Respuesta' => $clientes), 200);
+			$this->response(array($clientes), 200);
 		} else {
 			$this->response(array('Respuesta' => 'No se han ingresado clientes'), 200);
 		}
